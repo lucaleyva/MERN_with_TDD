9f5727ada546a0d1921c2f5489231bbc1732ca67
Luca Leyva <imlucaleyva@gmail.com>
2022-06-06 06:01:34 -0700

    added code to app.js and reinstalled packages

---
+++ Backend/app.test.js
@@ -0,0 +1,9 @@
+// app.test.js
+import { expect } from 'chai';
+import MessageApp from './app.js';
+describe('app', function () {
+  let testApp = new MessageApp();
+  it('app has messages', function () {
+    expect(testApp.messages).to.be.an('array');
+  });
+});
